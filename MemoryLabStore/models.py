34cc32e7536c50cc7ed5230c25a9f00e7980e812
Slim OURLISSENE <slim@DESKTOP-O907GDE.localdomain>
2023-02-18 19:41:07 +0100

    initial push

---
+++ MemoryLabStore/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+"""
+
+Product
+- name
+- price
+- description
+- image
+
+"""
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField(default=0.0)
+    description = models.TextField(default='', blank=True)
+    image = models.ImageField(upload_to='images', blank=True, null=True)
